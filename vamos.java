42b38304d3025763f9e4d3ee7729c54f72355c48
elalegrebandolero <adiacat063@g.educaand.es>
2024-09-27 18:33:05 +0200

    ampliacion

--- vamos.java
+++ vamos.java
@@ -1,6 +1,6 @@
 public class Vamos {
   public static void main (String[] args) {
     System.out.println("vamos mi sevilla");
-    System.out.println("vamos campeon");
+    System.out.println("vamos campeon,lololo");
   }
 }
